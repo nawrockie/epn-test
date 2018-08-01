41b9f1a29bf9bce5769e5d7830e821ddaf90c971
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2018-08-01 11:52:44 -0400

    Fixes bug reporting when an output file is empty vs does not exist

--- epn-test.pm
+++ epn-test.pm
@@ -237,7 +237,7 @@ sub test_DiffTwoFiles {
     }
   }
   else { 
-    $conclusion = ($out_file_exists) ? "FAIL [output file does not exist]" : "FAIL [output file exists but is empty]";
+    $conclusion = ($out_file_exists) ? "FAIL [output file exists but is empty]" : "FAIL [output file does not exist]";
   }
 
   ofile_OutputString($FH_HR->{"log"}, 1, "$conclusion\n");
